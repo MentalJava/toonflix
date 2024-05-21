0884220f67749504415ca19b487c550b1390fb13
Si Yujun <siuj0810@gmail.com>
2024-05-21 20:47:19 +0900

    jsonDecode

--- lib/models/webtoon_model.dart
+++ lib/models/webtoon_model.dart
@@ -1,8 +1,10 @@
 class WebtoonModel {
   final String title, thumb, id;
 
-  WebtoonModel.fromJson(Map<String, dynamic> json)
+  WebtoonModel.fromJson(
+      Map<String, dynamic>
+          json) // json 데이터 구조를 Map<String, dynamic> 데이터 타입으로 변경
       : title = json['title'],
         thumb = json['thumb'],
-        id = json['id'];
+        id = json['id']; // 받은 데이터들로 초기화
 }
--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -7,16 +7,16 @@ class ApiService {
   final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
   final String today = "today";
 
-  void getTodaysToons() async {
-    final url = Uri.parse("$baseUrl/$today");
+  Future<List<WebtoonModel>> getTodaysToons() async {
+    List<WebtoonModel> webtoonInstances = [];
+    final url = Uri.parse("$baseUrl/$today"); // String -> Uri 변환
     final response = await http.get(url); // 네임스페이스 사용
     if (response.statusCode == 200) {
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
-        final toon = WebtoonModel.fromJson(webtoon);
-        print(toon);
+        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
       }
-      return;
+      return webtoonInstances;
     }
     throw Error();
   }
