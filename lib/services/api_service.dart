86efa03c0691bf8af71eaf81f0fd4131bcb44bae
Si Yujun <siuj0810@gmail.com>
2024-05-22 20:44:50 +0900

    waitForWebToons

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,17 +4,21 @@ import 'package:http/http.dart' as http;
 import 'package:toonflix/models/webtoon_model.dart'; // 패키지에 네임스페이스 지정
 
 class ApiService {
-  final String baseUrl = "https://webtoon-crawler.nomadcoders.workers.dev";
-  final String today = "today";
+  static const String baseUrl =
+      "https://webtoon-crawler.nomadcoders.workers.dev";
+  static const String today = "today";
 
-  Future<List<WebtoonModel>> getTodaysToons() async {
+  static Future<List<WebtoonModel>> getTodaysToons() async {
     List<WebtoonModel> webtoonInstances = [];
     final url = Uri.parse("$baseUrl/$today"); // String -> Uri 변환
-    final response = await http.get(url); // 네임스페이스 사용
+    final response = await http
+        .get(url); // api에 http 요청 (Future타입 반환, 당장 완료 x, 마무리 후 저장), 네임스페이스 사용
     if (response.statusCode == 200) {
+      // response 성공 했을경우 body 부분 디코딩
       final List<dynamic> webtoons = jsonDecode(response.body);
       for (var webtoon in webtoons) {
-        webtoonInstances.add(WebtoonModel.fromJson(webtoon));
+        final instance = WebtoonModel.fromJson(webtoon);
+        webtoonInstances.add(instance);
       }
       return webtoonInstances;
     }
